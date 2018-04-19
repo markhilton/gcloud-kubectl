8e0dc3e5d50fc041e7ec64d051451afa805e77eb
Mark Hilton <mark@crunchgeek.com>
2018-04-19 19:16:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # gcloud-kubectl
-Google Cloud SDK with authorized access to Kubernetes cluster
+
+gcloud-kubectl is a Docker container that establishes connection to Kubernetes cluster running on Google Cloud end exposes SSH port to make it possible to execute `kubectl` remote commands.
+
+## Variables (required)
+
+`SSH_KEY` 
+public RSA key file authorized to connect to container via SSH port
+
+`KUBE_AUTH`
+Google service account json key file with privileges to manage Kubernetes
+
+`KUBE_NAME`
+Kubernetes cluster name to connect to
+
+`KUBE_ZONE`
+Kubernetes cluster location zone
+
+## Example use
+
+Start container by mounting the volume from local file system containing RSA key and Google service account json key (/config in this example) into /auth directory inside the container.
+
+```
+docker run --rm \
+    -v /config:/auth \
+    -e SSH_KEY=id_rsa.pub \
+    -e KUBE_AUTH=gcloud-auth.json \
+    -e KUBE_NAME=kubernetes \
+    -e KUBE_ZONE=us-central1-f \
+    -ti crunchgeek/gcloud-kubectl
+```
+
+### docker-compose.yml
+
+```
+version: "3"
+
+services:
+    gcloud_sdk:
+        image: crunchgeek/gcloud-kubectl
+        volumes:
+            - /config/:/auth
+        environment:
+            - SSH_KEY=id_rsa.pub
+            - KUBE_AUTH=gcloud-auth.json
+            - KUBE_NAME=kubernetes
+            - KUBE_ZONE=us-central1-f
+```
---
+++ init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# check for required env variables
+ERROR=0
+
+if [ -z "$SSH_KEY" ]       ; then echo -e "\033[1;91mERROR:\033[0m SSH_KEY env variable is required"   && ERROR=1; fi
+if [ -z "$KUBE_AUTH" ]     ; then echo -e "\033[1;91mERROR:\033[0m KUBE_AUTH env variable is required" && ERROR=1; fi
+if [ -z "$KUBE_ZONE" ]     ; then echo -e "\033[1;91mERROR:\033[0m KUBE_ZONE env variable is required" && ERROR=1; fi
+if [ -z "$KUBE_NAME" ]     ; then echo -e "\033[1;91mERROR:\033[0m KUBE_NAME env variable is required" && ERROR=1; fi
+
+# check if credentials files exist
+if [[ ! -f "/auth/$SSH_KEY"   ]] ; then echo -e "\033[1;91mERROR:\033[0m SSH public key file not found" && ERROR=1; fi
+if [[ ! -f "/auth/$KUBE_AUTH" ]] ; then echo -e "\033[1;91mERROR:\033[0m Google service account json key file not found" && ERROR=1; fi
+
+# stop script if errors detected
+if [ $ERROR -eq 1 ]; then exit 1; fi
+
+# authorize RSA key to connect to container
+mkdir -p /root/.ssh && \
+cat /auth/$SSH_KEY > /root/.ssh/authorized_keys && \
+chmod 600 /root/.ssh/authorized_keys
+
+export KUBE_PROJECT=$(cat /auth/$KUBE_AUTH | jq -r .project_id   >/dev/null 2>/dev/null)
+export KUBE_ACCOUNT=$(cat /auth/$KUBE_AUTH | jq -r .client_email >/dev/null 2>/dev/null)
+
+if [ -z "$KUBE_PROJECT" ] || [ -z "$KUBE_ACCOUNT" ]; then 
+	echo -e "\033[1;91mERROR:\033[0m invalid google service account json key"
+	exit 1
+fi
+
+echo "Google Cloud Project: [ $KUBE_PROJECT ]"
+echo "Google Cloud Account: [ $KUBE_ACCOUNT ]"
+echo "Kubernetes cluster:   [ $KUBE_NAME ]"
+
+# establish connection to Kubernetes cluster for kubectl
+gcloud auth activate-service-account $KUBE_ACCOUNT --key-file=/auth/$KUBE_AUTH && \
+gcloud container clusters get-credentials $KUBE_NAME --zone $KUBE_ZONE --project $KUBE_PROJECT && \
+printf "Testing connection...\n\n" && kubectl config view
+
+# start SSH server
+printf "\nAwaiting SSH connections...\n"
+/usr/sbin/sshd -D
